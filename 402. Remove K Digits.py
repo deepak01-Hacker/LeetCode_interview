f4a425fa42c7db1258ffa85a88c7c17d319fcbe5
Deepak <60144733+deepak01-Hacker@users.noreply.github.com>
2024-04-11 08:30:17 +0530

    Update 402. Remove K Digits.py

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -11,3 +11,29 @@ class Solution:
                res = min(res, util(num[:i] + num[i+1:], k-1))
             return res
         return str(util(num, k))
+
+# better approach 
+
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+
+        stack = []
+        for i in num:
+            while(stack and stack[-1] > i and k > 0):
+                stack.pop()
+                k -= 1
+            stack.append(i)
+
+        while(stack and k > 0):
+            stack.pop()
+            k -= 1
+
+        while(stack and stack[0] == "0" and stack.count('0') <= len(stack)):
+            stack.pop(0)
+        
+        if stack == []:
+            return "0"
+
+        return "".join(stack)
+
